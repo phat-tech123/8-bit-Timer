32bc2da6821212eefc416e430381ac80a86bb3b4
phat-tech123 <caovinhphat1904@gmail.com>
2025-06-10 11:22:08 +0700

    update

--- ClockSelect.v
+++ ClockSelect.v
@@ -1,4 +1,6 @@
-module ClockSelect(
+module ClockSelect#(
+	parameter CLK_SELECT_BIT_WIDTH = 3
+)(
 	//Internal clocks
 	input wire clk,
 
@@ -6,12 +8,56 @@ module ClockSelect(
 	input wire TMCI0,
 	input wire TMCI1,
 
+	//clock_select
+	input wire [CLK_SELECT_BIT_WIDTH-1:0] clock_select_0;
+	input wire [CLK_SELECT_BIT_WIDTH-1:0] clock_select_1;
+	input wire edge_select;
+
 	//output
-	output CounterClock1,
-	output CounterClock2
+	output wire CounterClock0,
+	output wire CounterClock1
 );
 
+    	wire clk_div2, clk_div8, clk_div32, clk_div64, clk_div1024, clk_div8192;
+	ClockDivider #(.DIVISOR(2))    div2   (.clk_in(clk), .clk_out(clk_div2));
+	ClockDivider #(.DIVISOR(8))    div8   (.clk_in(clk), .clk_out(clk_div8));
+	ClockDivider #(.DIVISOR(32))   div32  (.clk_in(clk), .clk_out(clk_div32));
+	ClockDivider #(.DIVISOR(64))   div64  (.clk_in(clk), .clk_out(clk_div64));
+	ClockDivider #(.DIVISOR(1024)) div1024(.clk_in(clk), .clk_out(clk_div1024));
+	ClockDivider #(.DIVISOR(8192)) div8192(.clk_in(clk), .clk_out(clk_div8192));
 
+	reg selected_clk_0;
+	always@(*) begin
+		case(clock_select)
+			3'b000: selected_clk_0 	= clk_div2;
+            		3'b001: selected_clk_0 	= clk_div8;
+			3'b010: selected_clk_0 	= clk_div32;
+			3'b011: selected_clk_0  = clk_div64;
+			3'b100: selected_clk_0 	= clk_div1024;
+			3'b101: selected_clk_0 	= clk_div8192;
+			3'b110: selected_clk_0 	= TMCI0;
+			3'b111: selected_clk_0 	= TMCI1;
+			default: selected_clk_0 = selected_clk;
+		endcase
+	end
+	
+	reg selected_clk_1;
+	always@(*) begin
+		case(clock_select)
+			3'b000: selected_clk_1 	= clk_div2;
+            		3'b001: selected_clk_1 	= clk_div8;
+			3'b010: selected_clk_1 	= clk_div32;
+			3'b011: selected_clk_1  = clk_div64;
+			3'b100: selected_clk_1 	= clk_div1024;
+			3'b101: selected_clk_1 	= clk_div8192;
+			3'b110: selected_clk_1 	= TMCI0;
+			3'b111: selected_clk_1 	= TMCI1;
+			default: selected_clk_1 = selected_clk;
+		endcase
+	end
+
+	assign CounterClock0 = selected_clk_0;
+	assign CounterClock1 = selected_clk_1;
 endmodule
 
 
@@ -22,5 +68,19 @@ module ClockDivider#(
 	input clk_in,
 	output reg clk_out
 );
+
+initial begin 
+	clk_out = 0;
+end
+
+reg [27:0] counter = 0;
+always@(posedge clk_in) begin
+	if(counter == DIVISOR-1) begin
+		clk_out <= ~clk_out;
+		counter <= 28'd0;
+	end else begin
+		counter <= counter + 1;
+	end
+end
 
 endmodule
--- LogicControl.v
+++ LogicControl.v
@@ -2,15 +2,16 @@ module ControlLogic#(
 	parameter BIT_WIDTH = 8,
 	parameter CLK_SELECT_BIT_WIDTH = 3
 )(
+	input clk,
 	input wire TMRI0,
 	input wire TMRI1,
 
-	inout wire [BIT_WIDTH-1:0] TCSR_0,
-	inout wire [BIT_WIDTH-1:0] TCSR_1,
 	input wire [BIT_WIDTH-1:0] TCR_0,
 	input wire [BIT_WIDTH-1:0] TCR_1,
 	input wire [BIT_WIDTH-1:0] TCCR_0,
 	input wire [BIT_WIDTH-1:0] TCCR_1,
+	inout wire [BIT_WIDTH-1:0] TCSR_0,
+	inout wire [BIT_WIDTH-1:0] TCSR_1,
 
 	input wire CompareMatchA0,
 	input wire CompareMatchA1,
@@ -21,18 +22,126 @@ module ControlLogic#(
 	output wire CounterClear0,
 	output wire CounterClear1,
 
-	output CMIA0,
-	output CMIA1,
-	output CMIB0,
-	output CMIB1,
-	output OVI0,
-	output OVI1,
+	output reg CMIA0,
+	output reg CMIA1,
+	output reg CMIB0,
+	output reg CMIB1,
+	output reg OVI0,
+	output reg OVI1,
 
-	output TMO0,
-	output TMO1,
-	output ADC_REQUEST,
+	output reg TMO0,
+	output reg TMO1,
+	output reg ADC_REQUEST,
 
-	output wire [CLK_SELECT_BIT_WIDTH-1:0] clock_select
+	output reg [CLK_SELECT_BIT_WIDTH-1:0] clock_select
 );
+
+/*
+// TCR
+wire CMIEB_0 = TCR_0[7];
+wire CMIEA_0;
+wire OVIE_0;
+wire CCLR1_0;
+wire CCLR0_0;
+wire CKS2_0;
+wire CKS1_0;
+wire CKS0_0;
+assign CMIEB_0 	= TCR_0[7];
+assign CMIEA_0 	= TCR_0[6];
+assign OVIE_0 	= TCR_0[5];
+assign CCLR0_0 	= TCR_0[4];
+assign CCLR1_0 	= TCR_0[3];
+assign CKS2_0 	= TCR_0[2];
+assign CKS1_0 	= TCR_0[1];
+assign CKS0_0 	= TCR_0[0];
+
+wire CMIEB_1;
+wire CMIEA_1;
+wire OVIE_1;
+wire CCLR1_1;
+wire CCLR0_1;
+wire CKS2_1;
+wire CKS1_1;
+wire CKS0_1;
+assign CMIEB_1 	= TCR_1[7];
+assign CMIEA_1 	= TCR_1[6];
+assign OVIE_1 	= TCR_1[5];
+assign CCLR0_1 	= TCR_1[4];
+assign CCLR1_1 	= TCR_1[3];
+assign CKS2_1 	= TCR_1[2];
+assign CKS1_1 	= TCR_1[1];
+assign CKS0_1 	= TCR_1[0];
+*/
+
+//TCR
+assign CMIEB_0 	= TCR_0[7];
+assign CMIEA_0 	= TCR_0[6];
+assign OVIE_0 	= TCR_0[5];
+assign CCLR0_0 	= TCR_0[4];
+assign CCLR1_0 	= TCR_0[3];
+assign CKS2_0 	= TCR_0[2];
+assign CKS1_0 	= TCR_0[1];
+assign CKS0_0 	= TCR_0[0];
+
+assign CMIEB_1 	= TCR_1[7];
+assign CMIEA_1 	= TCR_1[6];
+assign OVIE_1 	= TCR_1[5];
+assign CCLR0_1 	= TCR_1[4];
+assign CCLR1_1 	= TCR_1[3];
+assign CKS2_1 	= TCR_1[2];
+assign CKS1_1 	= TCR_1[1];
+assign CKS0_1 	= TCR_1[0];
+
+//TCCR
+assign TMRIS_0 	= TCCR_0[3];
+assign ICKS1_0 	= TCCR_0[1];
+assign ICKS0_0 	= TCCR_0[0];
+
+assign TMRIS_1 	= TCCR_1[3];
+assign ICKS1_1 	= TCCR_1[1];
+assign ICKS0_1 	= TCCR_1[0];
+
+//TCSR
+
+always@(posedge clk) begin
+	//Channel 0:
+	if(CMIEB_0 == 1) 
+		CMIB0 = 1;
+	else 	
+		CMIB0 = 0;
+
+	if(CMIEA_0 == 1)
+		CMIA0 = 1;
+	else 	
+		CMIA0 = 0;
+
+	if(OVIE_0 == 1)
+		OVI0 = 1;
+	else 	
+		OVI0 = 0;
+
+	//Channel 1:
+	if(CMIEB_1 == 1) 
+		CMIB1 = 1;
+	else 	
+		CMIB1 = 0;
+
+	if(CMIEA_1 == 1)
+		CMIA1 = 1;
+	else 	
+		CMIA1 = 0;
+
+	if(OVIE_1 == 1)
+		OVI1 = 1;
+	else 	
+		OVI1 = 0;
+end
+
+
+assign counterClear_0 = (TMRIS_0) ? TMRI0 : (posedge clk & TMRI0); 
+assign counterClear_1 = (TMRIS_1) ? TMRI1 : (posedge clk & TMRI1); 
+
+
+
 
 endmodule
